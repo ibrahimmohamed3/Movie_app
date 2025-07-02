8d4de0607b03861c18213c69edf72c12424e32fc
ibrahim <ibra39167271@gmail.com>
2025-07-02 19:44:07 +0300

    add the form functionality

--- movie_app/src/App.jsx
+++ movie_app/src/App.jsx
@@ -1,10 +1,13 @@
-
-import './App.css'
+import "./App.css";
+import MovieCard from "./Components/MovieCard";
+import Home from "./Components/pages/Home";
 
 function App() {
- return (
-   <div></div>
-  )
+  return (
+    <div>
+     <Home/>
+    </div>
+  );
 }
 
-export default App
+export default App;
